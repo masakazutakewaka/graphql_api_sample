b8843188452590e01c68655ea7ab0a81c8b11286
masakazutakewaka <takewakamma@gmail.com>
2018-12-11 09:18:54 +0900

    syntax for plural field

--- app/graphql/types/snap_type.rb
+++ app/graphql/types/snap_type.rb
@@ -2,6 +2,6 @@ module Types
   class SnapType < Types::BaseObject
     field :id, Integer, null: true
     field :user_id, Integer, null: true
-    field :items, Types::ItemType, null: true
+    field :items, [Types::ItemType], null: true
   end
 end
