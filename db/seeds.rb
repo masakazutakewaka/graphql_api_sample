d42f71b844a6c0ebaf09651a6debc66909079fee
masakazutakewaka <takewakamma@gmail.com>
2018-12-11 09:13:50 +0900

    auto generate data with factorybot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#   ※Not Recommended to use Factory_Bot here ※
+#   https://robots.thoughtbot.com/factory_girl-for-seed-data
+#
+require 'factory_bot'
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    sex { :man }
+  end
+
+  factory :snap do
+    sequence(:user_id) { |n| n }
+  end
+end
+
+FactoryBot.create_list(:user, 10)
+FactoryBot.create_list(:snap, 10)
